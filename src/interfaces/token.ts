aeb93b215b6a3464b8e0f2bfba7b0d5f8f527254
fahim <fahim.shoumik@gmail.com>
2023-10-09 15:47:16 +0600

    cross cookie identifier added

---
+++ src/interfaces/token.ts
@@ -0,0 +1,14 @@
+export interface SystemTokenParam {
+    version: number
+    type: string
+    app_data: any
+    access_key: string
+    role: string
+    room_id: string
+    exp: number
+    jti: string
+    iat: number
+    iss: string
+    nbf: number
+    sub: string
+}
